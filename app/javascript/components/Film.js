b542196e47585b26020b4b16c6d9a0bc1ca9f1fd
orepc <gopc@orepc.local>
2019-08-04 20:54:25 +0900

    React動作確認、不要コード削除

---
+++ app/javascript/components/Film.js
@@ -0,0 +1,11 @@
+import React from "react"
+import PropTypes from "prop-types"
+class Film extends React.Component {
+  render () {
+    return (
+      <p>helloreact </p>
+    );
+  }
+}
+
+export default Film
