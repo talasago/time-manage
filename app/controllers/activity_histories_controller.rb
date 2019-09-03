816262e20811713dc4990494a0585249be375334
orepc <gopc@orepc.local>
2019-09-03 14:48:56 +0900

    登録済みの行動履歴をカレンダーから選択すると入力フォームが表示される様に実装

--- app/controllers/activity_histories_controller.rb
+++ app/controllers/activity_histories_controller.rb
@@ -13,9 +13,8 @@ class ActivityHistoriesController < ApplicationController
     respond_to do |format|
       if act_history.save
         flash[:success] = "登録できました"
-        format.json { render json: act_history.to_json, status: :created }
+        format.json { render json: act_history.to_json, status: :created } #無意味なrender。js側でリロードしているため
       else
-        flash[:danger] = "登録することができませんでした"
         format.json {render json: act_history.errors.messages.to_json, status: :unprocessable_entity }
       end
     end
@@ -31,6 +30,31 @@ class ActivityHistoriesController < ApplicationController
     respond_to do |format|
       format.json {
         render json: act_histories.to_json
+      }
+    end
+  end
+
+  def edit
+    json_str  = request.body.read     # リクエストのJSON
+    json_hash = JSON.parse(json_str,symbolize_names: true)
+
+    act_history = current_user.activity_historys.select(
+      "activity_name,
+       to_char(from_time, 'YYYY-MM-DD') AS from_ymd,
+       to_char(from_time, 'HH24:MI')      AS from_hm,
+       to_char(to_time, 'YYYY-MM-DD')   AS to_ymd,
+       to_char(to_time, 'HH24:MI')      AS to_hm,
+       remarks"
+    ).where(
+      "activity_name = ? and from_time = ? and to_time = ?",
+      json_hash[:title],
+      DateTime.parse(json_hash[:start]),
+      DateTime.parse(json_hash[:end])
+    ).as_json(only: [:activity_name, :from_ymd, :from_hm, :toYMD, :to_ymd, :to_hm, :remarks])
+
+    respond_to do |format|
+      format.json {
+        render json: act_history.to_json
       }
     end
   end
