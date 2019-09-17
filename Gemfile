6f7f617e94c797b5cf1837ae8f8e4f7fd22b91d1
orePC <lyricaldragons@gmail.com>
2019-09-17 21:48:47 +0900

    /users/:idにアクセスした際にログインを要求するように修正

--- Gemfile
+++ Gemfile
@@ -60,6 +60,7 @@ group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'rspec-rails'
   gem 'spring-commands-rspec'
+  gem 'pry-rails'
 end
 
 group :test do
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: %i[show]
+
   def new
     @user = User.new
   end
@@ -22,5 +24,13 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :password, :password_confirmation)
+  end
+
+  # ユーザーのログインを確認する
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "ログインしてください"
+      redirect_to login_url
+    end
   end
 end
