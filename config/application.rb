7f72dce0493399d18bfc021abeeabc2c025736da
orePC <lyricaldragons@gmail.com>
2019-09-21 21:17:37 +0900

    time_zone変更

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,8 @@ module TimeManageApp
     config.generators.system_tests = nil
 
     config.i18n.default_locale = :ja
+
+    config.time_zone = "Tokyo"
+    config.active_record.default_timezone = :local
   end
 end
